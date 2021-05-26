1b7f1a247e1cec62c617855fceb8ddda85a2852c
Matthew Murphy <mattjmurphy98@gmail.com>
2021-05-25 22:25:35 -0400

    Fixed duplicate model fields error

--- home/models.py
+++ home/models.py
@@ -32,8 +32,7 @@ class Order(models.Model):
 
     order_isreserved = models.BooleanField(default=False, blank=True, null=True)
 
-    order_isreserved = models.BooleanField(default=False, blank=True)
-    order_ismarked = models.BooleanField(default=False, blank=True)
+   # order_ismarked = models.BooleanField(default=False, blank=True)
 
 
     def sort_by_od(self):
